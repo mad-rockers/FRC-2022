63f92dcd7422e89748c23cfbb6e9f8135bfb3572
Joe McConnell <joe.mcconnell10@gmail.com>
2023-01-28 10:26:55 -0600

    RGB values result in a color

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,12 +7,14 @@
 
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/smartdashboard.h>
+#include <frc/util/color.h>
 
 #include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
 
 /**
- * This is a simple example to show the values that can be read from the REV
- * Color Sensor V3
+ * This is a simple example to show how the REV Color Sensor V3 can be used to
+ * detect various colors.
  */
 class Robot : public frc::TimedRobot {
   /**
@@ -27,7 +29,31 @@ class Robot : public frc::TimedRobot {
    */
   rev::ColorSensorV3 m_colorSensor{i2cPort};
 
+  /**
+   * A Rev Color Match object is used to register and detect known colors. This can 
+   * be calibrated ahead of time or during operation.
+   * 
+   * This object uses a simple euclidian distance to estimate the closest match
+   * with given confidence range.
+   */
+  rev::ColorMatch m_colorMatcher;
+
+  /**
+   * Note: Any example colors should be calibrated as the user needs, these
+   * are here as a basic example.
+   */
+  static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+  static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+  static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+  static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
+
  public:
+  void RobotInit() {
+    m_colorMatcher.AddColorMatch(kBlueTarget);
+    m_colorMatcher.AddColorMatch(kGreenTarget);
+    m_colorMatcher.AddColorMatch(kRedTarget);
+    m_colorMatcher.AddColorMatch(kYellowTarget);
+  }
   void RobotPeriodic() {
     /**
      * The method GetColor() returns a normalized color value from the sensor and can be
@@ -42,9 +68,23 @@ class Robot : public frc::TimedRobot {
     frc::Color detectedColor = m_colorSensor.GetColor();
 
     /**
-     * The sensor returns a raw IR value of the infrared light detected.
+     * Run the color match algorithm on our detected color
      */
-    double IR = m_colorSensor.GetIR();
+    std::string colorString;
+    double confidence = 0.0;
+    frc::Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence);
+
+    if (matchedColor == kBlueTarget) {
+      colorString = "Blue";
+    } else if (matchedColor == kRedTarget) {
+      colorString = "Red";
+    } else if (matchedColor == kGreenTarget) {
+      colorString = "Green";
+    } else if (matchedColor == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
 
     /**
      * Open Smart Dashboard or Shuffleboard to see the color detected by the 
@@ -53,22 +93,9 @@ class Robot : public frc::TimedRobot {
     frc::SmartDashboard::PutNumber("Red", detectedColor.red);
     frc::SmartDashboard::PutNumber("Green", detectedColor.green);
     frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
-    frc::SmartDashboard::PutNumber("IR", IR);
+    frc::SmartDashboard::PutNumber("Confidence", confidence);
+    frc::SmartDashboard::PutString("Detected Color", colorString);
 
-    /**
-     * In addition to RGB IR values, the color sensor can also return an 
-     * infrared proximity value. The chip contains an IR led which will emit
-     * IR pulses and measure the intensity of the return. When an object is 
-     * close the value of the proximity will be large (max 2047 with default
-     * settings) and will approach zero when the object is far away.
-     * 
-     * Proximity can be used to roughly approximate the distance of an object
-     * or provide a threshold for when an object is close enough to provide
-     * accurate color values.
-     */
-    uint32_t proximity = m_colorSensor.GetProximity();
-
-    frc::SmartDashboard::PutNumber("Proximity", proximity);
   }
 };
 
