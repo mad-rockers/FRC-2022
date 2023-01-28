4ddd91fde18023af6c88a4ee54aa9e4e6a4d64e6
Joe McConnell <joe.mcconnell10@gmail.com>
2023-01-28 09:59:22 -0600

    first try with color sensor

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,65 +1,77 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-#include "Robot.h"
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/smartdashboard.h>
 
-#include <fmt/core.h>
-
-#include <frc/smartdashboard/SmartDashboard.h>
-
-using namespace frc;
-
-void Robot::RobotInit() {}
+#include "rev/ColorSensorV3.h"
 
 /**
- * This function is called every 20 ms, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
+ * This is a simple example to show the values that can be read from the REV
+ * Color Sensor V3
  */
-void Robot::RobotPeriodic() {}
+class Robot : public frc::TimedRobot {
+  /**
+   * Change the I2C port below to match the connection of your color sensor
+   */
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {}
+  /**
+   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
+   * parameter. The device will be automatically initialized with default 
+   * parameters.
+   */
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
 
-void Robot::AutonomousPeriodic() {}
+ public:
+  void RobotPeriodic() {
+    /**
+     * The method GetColor() returns a normalized color value from the sensor and can be
+     * useful if outputting the color to an RGB LED or similar. To
+     * read the raw color, use GetRawColor().
+     * 
+     * The color sensor works best when within a few inches from an object in
+     * well lit conditions (the built in LED is a big help here!). The farther
+     * an object is the more light from the surroundings will bleed into the 
+     * measurements and make it difficult to accurately determine its color.
+     */
+    frc::Color detectedColor = m_colorSensor.GetColor();
 
-void Robot::TeleopInit() {}
+    /**
+     * The sensor returns a raw IR value of the infrared light detected.
+     */
+    double IR = m_colorSensor.GetIR();
 
-void Robot::TeleopPeriodic() {
-  const float brake = 0.01;
-  front_l.SetInverted(true);
-  front_r.SetInverted(true);
-  drivetrain.DriveCartesian(-controller.GetLeftX() + brake, controller.GetLeftY() + brake, -controller.GetRightX() + brake);
-}
+    /**
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+     * sensor.
+     */
+    frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+    frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+    frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+    frc::SmartDashboard::PutNumber("IR", IR);
 
-void Robot::DisabledInit() {}
+    /**
+     * In addition to RGB IR values, the color sensor can also return an 
+     * infrared proximity value. The chip contains an IR led which will emit
+     * IR pulses and measure the intensity of the return. When an object is 
+     * close the value of the proximity will be large (max 2047 with default
+     * settings) and will approach zero when the object is far away.
+     * 
+     * Proximity can be used to roughly approximate the distance of an object
+     * or provide a threshold for when an object is close enough to provide
+     * accurate color values.
+     */
+    uint32_t proximity = m_colorSensor.GetProximity();
 
-void Robot::DisabledPeriodic() {}
-
-void Robot::TestInit() {}
-
-void Robot::TestPeriodic() {}
-
-void Robot::SimulationInit() {}
-
-void Robot::SimulationPeriodic() {}
+    frc::SmartDashboard::PutNumber("Proximity", proximity);
+  }
+};
 
 #ifndef RUNNING_FRC_TESTS
-int main() {
-  return frc::StartRobot<Robot>();
-}
+int main() { return frc::StartRobot<Robot>(); }
 #endif
